64ad1914e5ed00d833ee5c3593bf5540efd27ed5
Debian live <eee1@i.ua>
2019-02-19 18:03:58 +0200

    all code in index.tsx, ver 0.2.1 (J219)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import * as App from './App';
+//import * as App from './App';
 
-ReactDOM.render(<App.Main />, document.getElementById('app'));
+const sVersion = "ver 0.2.1 (J219)";
+
+class Main extends Component {
+  render() {
+    let sVerInfo = `${sVersion}`;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {sVerInfo}
+        </header>
+      </div>
+    );
+  }
+}
+
+interface IProps {}
+interface IState {}
+class Hello extends Component<IProps, IState> {
+  render() {
+    return (
+      <div className="Elem1">
+          <h3>It is just 'hello' test</h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('app'));
+ReactDOM.render(<Hello />, document.getElementById('hello'));
+

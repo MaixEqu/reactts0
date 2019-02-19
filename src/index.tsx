c7d84ed0565cae50310c843849a1233fec7deebb
PC home 1 <eee1@i.ua>
2019-02-20 01:00:44 +0200

    fill TA from file not ok, ver 0.3.9 (J219)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const sVersion = "ver 0.3.8 (J219)";
+const sVersion = "ver 0.3.9 (J219)";
 
 class Main extends Component {
   render() {
@@ -64,21 +64,57 @@ const sGetTxt = async (path: string): Promise<void> => {
   }
 }
 
+const sGetTxt_ = async (path: string, callback: Function) => {
+  try {
+    const response = await fetch(path);
+    const text = await response.text();
+    console.log(text);
+    callback(text)
+  } catch(error) {
+    console.error(error);
+  }
+}
 
-class TextAreaCounter extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this._onChange = this._onChange.bind(this);
+interface IPropsTA {
+  text?: string;
+}
+interface IStateTA {
+  text?: string
+}
+class TextAreaCounter extends Component<IPropsTA, IStateTA> {
+  constructor(props: IPropsTA) {
+    super(props)
     const sText = this.props.text || "no text"
-    this.state = {text: sText,};
-    //this.setState({'text': sText});
+    this.state = {text: sText,}
+    const sFPath = location.href + '/data/1.txt'
+    console.log("path: " + sFPath);
+    this.sGetTxt_2(sFPath) //, this._callBack)
+    this._onChange = this._onChange.bind(this);
+    //this._callBack = this._callBack.bind(this);
+    //this.state = {}
+  }
+
+  sGetTxt_2 = async (path: string): Promise<void> => {
+    try {
+      const response = await fetch(path);
+      const text = await response.text();
+      console.log("cb2:\n" + text);
+      this.setState({text});
+      //this.state = {text: text,}
+    } catch(error) {
+      console.error(error);
+    }
   }
 
   _onChange(ev: React.ChangeEvent<HTMLTextAreaElement>) {
     //console.log(ev.target.value);
     //console.log("5 " + ev);
-    this.setState({'text': ev.target.value});
+    //this.setState({'text': ev.target.value});
   };
+
+  _callBack(sData: string) {
+    //this.setState({'text': sData});
+  }
 
   render() {
     //const sText = this.props.text || 'no state'
@@ -92,11 +128,11 @@ class TextAreaCounter extends Component<IProps, IState> {
   }
 } 
 
-const Hello1 = <Hello path = "1.txt" text = "one text..." />
+// const Hello1 = <Hello path = "1.txt" text = "one text..." />
 
 ReactDOM.render(<Main />, document.getElementById('app'));
 // ReactDOM.render(Hello1, document.getElementById('hello1'));
 // ReactDOM.render(<Hello path = "2.txt" />, document.getElementById('hello2'));
-ReactDOM.render(<TextAreaCounter text = "text in textarea.." />, document.getElementById('textarea1'));
+ReactDOM.render(<TextAreaCounter />, document.getElementById('textarea1'));
 
 
